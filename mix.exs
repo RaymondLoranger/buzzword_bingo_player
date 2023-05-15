92464c8be2317315e7ddf93513926446678b0edd
RaymondLoranger <Raymond.Loranger@bell.net>
2023-05-14 21:45:00 -0400

    0.1.1

--- mix.exs
+++ mix.exs
@@ -4,10 +4,33 @@ defmodule Buzzword.Bingo.Player.MixProject do
   def project do
     [
       app: :buzzword_bingo_player,
-      version: "0.1.20",
+      version: "0.1.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      name: "Buzzword Bingo Player",
+      source_url: source_url(),
+      description: description(),
+      package: package(),
       deps: deps()
+    ]
+  end
+
+  defp source_url do
+    "https://github.com/RaymondLoranger/buzzword_bingo_player"
+  end
+
+  defp description do
+    """
+    Player in the Multi-Player Buzzword Bingo game.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Raymond Loranger"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => source_url()}
     ]
   end
 
